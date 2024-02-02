7e81e872333e96c0520a3fe5fc84f9410e8176d3
Varun Gotmare <varunpgotmare@gmail.com>
2024-02-02 18:50:25 +0530

    update: navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -252,7 +252,7 @@ const Navbar = () => {
           
         </NavItems>
         <ButtonContainer>
-          <GitHubButton href={Bio.github} target="_blank">Github Profile</GitHubButton>
+          <GitHubButton href={Bio.insta} target="_blank">Instagram Handle</GitHubButton>
         </ButtonContainer>
         {
           isOpen &&
